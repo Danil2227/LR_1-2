7429e156a7e3ae3a5f5bcd8a985ac8b797db4aba
Danil2227 <144226545+Danil2227@users.noreply.github.com>
2023-09-17 18:39:11 +0300

    Add files via upload

---
+++ b4_n26.pas
@@ -0,0 +1,13 @@
+﻿program b4_26;
+  var a,b,c:integer;
+begin
+  writeln('Введите a');
+ read(a);
+  writeln('Введите b');
+ read(b);
+  writeln('Введите c');
+ read(c);
+ if  ((a+b)>c) and ((a+c)>b) and ((b+c)>a)
+ then write('Существует') else write('Не существует');
+   
+end.
